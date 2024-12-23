d8f77733b1b5f0601b0af682f42826e1ef714b14
xm3good <136872449@qq.com>
2024-12-23 21:55:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,1 +1,17 @@
-qqq
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*OpenWrt-mihomo\)/\1/' feeds.conf.default
+
+# Add a feed source
+echo 'src-git OpenWrt-mihomo https://github.com/morytyann/OpenWrt-mihomo' >>feeds.conf.default
